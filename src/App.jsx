37c8f7e1ab59629e894d8cd818d0ea43e649a10f
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-04-03 20:03:03 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ducimus fuga ipsa, eligendi impedit repellat porro laborum tenetur in. Doloremque asperiores fugiat repellendus accusamus eius sit! Doloribus nesciunt earum reprehenderit.</p>
+    <h3>Lorem, ipsum dolor.</h3>
+    </div>
+  );
+};
+
+export default App;
