f6efda4a99cd0bca4267fb5eb1cc88137dfdff66
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-04-04 20:03:32 +0500

    aaaadddddddddddd

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ducimus fuga ipsa, eligendi impedit repellat porro laborum tenetur in. Doloremque asperiores fugiat repellendus accusamus eius sit! Doloribus nesciunt earum reprehenderit.</p>
+      <p className=" bg-[black]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ducimus fuga ipsa, eligendi impedit repellat porro laborum tenetur in. Doloremque asperiores fugiat repellendus accusamus eius sit! Doloribus nesciunt earum reprehenderit.</p>
     <h3>Lorem, ipsum dolor.</h3>
+    <button>send</button>
     </div>
   );
 };
